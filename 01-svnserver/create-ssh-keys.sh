54e6db3549a52fa3332015fda2f111a02b9d7749
Oliver kfsone Smith <oliver@kfs.org>
2022-03-29 17:56:01 -0700

    Fix file eols on windows

--- 01-svnserver/create-ssh-keys.sh
+++ 01-svnserver/create-ssh-keys.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 function die() {
-	echo -e "\e[31;1mERROR: $*\e[0m"
-	exit 22
+  echo -e "\e[31;1mERROR: $*\e[0m"
+  exit 22
 }
 
 rm -rf /tmp/new-ssh-keys
@@ -13,7 +13,7 @@ for key_type in rsa dsa ecdsa ed25519; do
     for key in blank keyed; do
       key_arg=$([ "$key" == "blank" ] && echo "" || echo "$key")
       ssh-keygen -f "/tmp/new-ssh-keys/id-${key_type}-${mode}-${key}" -t $key_type $mode_arg -N "$key_arg" || \
-		  die "failed generating $key_type $mode $key key."
+      die "failed generating $key_type $mode $key key."
     done
   done
 done
